d5cdb861bfa950fe93e907f0a2b2e2158a11ae61
gchu <gchu647@gmail.com>
2018-08-06 22:02:20 -1000

    have basic get and post route for gallery

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const knex = require('../db/knex');
+const Photo = require('../db/models/Photo');
 
-router.get('/', (req, res) => {
-  res.send('gallery testing!!');
-});
+router.route('/')
+  .post((req, res) => {
+    let { author, link, description } = req.body;
+    author = author.trim();
+    link = link.trim();
+    description = description.trim();
+
+    return new Photo({ author, link, description })
+      .save()
+      .then( photo => {
+        return res.json(photo);
+      })
+      .catch(err => {
+        return res.json({ message: err.message });
+      });
+  })
+
+  .get((req, res) => {
+    return Photo.fetchAll()
+      .then( photos => {
+        console.log('photos: ', photos);
+        res.json(photos);
+      })
+      .catch(err => {
+        return res.json({ message: err.message });
+      })
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const exphbs = require('express-handlebars');
 const gallery = require('./routes/gallery');
 
+//-- SET UP PORT, BODY-PARSER, & HBS --//
 const PORT = process.env.PORT || 8060;
 app.use(bodyParser.urlencoded({ extended: true}));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 
+//-----------------------------------//
 app.get('/', (req, res) => {
   res.send('smoke test!');
 });
 
 app.use('/gallery', gallery);
+
+
+app.get('*', (req, res) => {
+  res.status(404).render('404', {'message': 'Page not found!'});
+})
+
+app.use(function(err, req, res, next) {
+  console.err(err.stack);
+  res.status(500).send('Something broke on the server side');
+})
 
 app.listen(PORT, () => {
   console.log(`server is listening on ${PORT}`);
